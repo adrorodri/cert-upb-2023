97063fcf577cf836e502f90b6e751be1d30f9cc6
Adrian Rodriguez <adrian.rodriguez@mojix.com>
2023-04-03 09:11:39 -0400

    Nuevos iconos app

--- app/src/main/java/com/upb/certupb2023/MainActivity.kt
+++ app/src/main/java/com/upb/certupb2023/MainActivity.kt
@@ -2,10 +2,20 @@ package com.upb.certupb2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    var num = 1
+    companion object {
+        const val TAG = "MainActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "Layout has loaded")
+
+        if (num == 2) {
+            Log.d(TAG, "Numero es 2")
+        }
     }
 }
